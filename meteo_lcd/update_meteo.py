a04a6fa1f1bbf07ef82bfed23ac3d092a32144a7
Janusz Kozerski <janusz.kozerski@gmail.com>
2018-07-12 13:12:44 +0200

    Add graph plot
    * Add plot graph in update_meteo script based on
      historical data from meteo.log file.
    * Add index.html file - ifames: meteo info,
      temp out graph, humid out graph, dew point out
      graph, pressure graph
    * Minor fixes in meteo.html file

--- meteo_lcd/update_meteo.py
+++ meteo_lcd/update_meteo.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 
-import re
+# install:
+# pip install python-dateutil
+# pip install plotly
+
+import re #regular expression
 from shutil import move
 from os import remove
 from math import sqrt, floor
 from datetime import datetime
+
+
+# Needed for drawing a plot
+import dateutil.parser
+import plotly
+#import plotly.plotly as py
+#import plotly.graph_objs as go
+
+# for testing purpose
+import random
 
 
 #www_meteo_path     = "/var/www/html/index.html"
@@ -64,6 +78,8 @@ def get_dew_point(temp, humid):
 	tmp = sqrt(sqrt(sqrt( float(humid)/100.0 ))) * (112.0 + (0.9 * float(temp))) + (0.1 * float(temp)) - 112.0;
 	return floor(tmp + 0.5);
 
+# Log data to file
+# We can use this data later to draw a plot
 def log_to_file(temp_in, humid_in, dew_in, temp_out, humid_out, dew_out, pressure):
 	lf = open(log_file_path, "a");
 	t = datetime.now()
@@ -71,6 +87,46 @@ def log_to_file(temp_in, humid_in, dew_in, temp_out, humid_out, dew_out, pressur
 	new_line = t.isoformat() + ";" + str(temp_in) + ";" + str(humid_in) + ";" + str(dew_in) + ";" + str(temp_out) + ";" + str(humid_out) + ";" + str(dew_out) + ";" + str(pressure) + "\n"
 	lf.write(new_line)
 	lf.close()
+
+# Converts a string back the datetime structure
+def getDateTimeFromISO8601String(s):
+    d = dateutil.parser.parse(s)
+    return d
+
+# Draw a plot
+def draw_plot():
+	lf = open(log_file_path, "r");
+	t = []; # time for plot
+	t_out = []; # temp out for plot
+	h_out = []; # humid out for plot
+	d_out = []; # dew point for plot
+	p_out = []; # pressure for plot
+	for line in lf:
+		time, temp_in, humid_in, dew_in, temp_out, humid_out, dew_out, pressure = str(line).split(";")
+		t.append(getDateTimeFromISO8601String(time))
+		t_out.append(float(temp_out))
+		h_out.append(float(humid_out))
+		d_out.append(float(dew_out))
+		p_out.append(float(pressure))
+	lf.close()
+	# Create datasets
+	data_temp  = [plotly.graph_objs.Scatter(x=t, y=t_out)]
+	data_humid = [plotly.graph_objs.Scatter(x=t, y=h_out)]
+	data_dew   = [plotly.graph_objs.Scatter(x=t, y=d_out)]
+	data_press = [plotly.graph_objs.Scatter(x=t, y=p_out)]
+	
+	#plotly.offline.plot(data, show_link=True, link_text='Export to plot.ly', validate=True, output_type='file', include_plotlyjs=True, filename='temp_out.html', auto_open=False, image=None, image_filename='plot_image', image_width=800, image_height=600, config=None)
+	
+	# draw temp out plot into html file
+	plotly.offline.plot(data_temp, show_link=False, link_text='Export to plot.ly', validate=False, output_type='file', include_plotlyjs=True, filename='temp_out.html', auto_open=False, image=None, image_filename='plot_image', image_width=800, image_height=600, config=None)
+	# draw humid out plot into html file
+	plotly.offline.plot(data_humid, show_link=False, link_text='Export to plot.ly', validate=False, output_type='file', include_plotlyjs=True, filename='humid_out.html', auto_open=False, image=None, image_filename='plot_image', image_width=800, image_height=600, config=None)
+	# draw dew point out plot into html file
+	plotly.offline.plot(data_humid, show_link=False, link_text='Export to plot.ly', validate=False, output_type='file', include_plotlyjs=True, filename='dew_out.html', auto_open=False, image=None, image_filename='plot_image', image_width=800, image_height=600, config=None)
+	# draw pressure plot into html file
+	plotly.offline.plot(data_humid, show_link=False, link_text='Export to plot.ly', validate=False, output_type='file', include_plotlyjs=True, filename='press_out.html', auto_open=False, image=None, image_filename='plot_image', image_width=800, image_height=600, config=None)
+	return
+
 
 # Update web data:
 def update_meteo_data(data):
@@ -110,7 +166,9 @@ def update_meteo_data(data):
 	move(www_meteo_path_tmp, www_meteo_path)
 	# Save data in log file
 	log_to_file(temp_in, humid_in, dew_in, temp_out, humid_out, dew_out, pressure);
+	# Draw a plot
+	draw_plot()
 
 # Main program
-update_meteo_data("30.0; 60; -20.5; 90; 1013.3");
+update_meteo_data("30.0; 60; " + str(random.randint(-20, 30)) + "; 90; 1013.3");
 
